c6b5073f41cc9167765579f82920df9afd4f531d
Jack Gao <jackgao1540@gmail.com>
2022-06-07 19:32:33 -0400

    removed another useless file

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-  for (int i = 0; i < nodes.size(); i++){
-        cout << i + 1 << "'s connections: " << endl;
-        for (int j = 0; j < nodes[i].connections.size(); j++) {
-            cout << nodes[i].connections[j].index << ' ';
-        }
-        cout << endl;
